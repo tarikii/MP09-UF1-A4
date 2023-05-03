f931a54c8d045bfa237c190592789775824b538d
tarikii <aabouchazougarthtarik@elpuig.xeill.net>
2023-05-03 23:42:16 +0200

    0,78

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        xifrarDesxifrarCodi111();
-        xifrarDesxifrarCodi112();
-    }
+        Scanner scanner = new Scanner(System.in);
+        boolean sortir = false;
 
-    public static void xifrarDesxifrarCodi111() throws IOException {
-        String rutaArxiu = "src/clausA4.txt";
+        while (!sortir){
+            System.out.println("Que vols fer: ");
+            System.out.println("1. Xifrar i Desxifrar text pla amb clau generada.");
+            System.out.println("2. Xifrar i Desxifrar text pla amb clau generada a partir d'un password.");
+            System.out.println("3. Executar alguns metodes que proporciona la classe SecretKey.");
+            System.out.println("4. Desxifrar el text de la opció 2 un altre vegada, pero amb una contrasenya incorrecta.");
+            System.out.println("5. Desxifrar la contrasenya correcta i el missatge del arxiu textamagat.crypt.");
+            System.out.println("0. Sortir.");
+            int opcio = scanner.nextInt();
+            if(opcio == 1){
+                UtilitatsXifrar.xifrarDesxifrarCodi111();
+            }
 
-        byte[] textClar = Files.readAllBytes(Paths.get(rutaArxiu));
-        int keySize = 128;
+            else if(opcio == 2){
+                UtilitatsXifrar.xifrarDesxifrarCodi112();
+            }
 
+            else if(opcio == 3){
+                UtilitatsXifrar.methodsSecretKey();
+            }
 
-        SecretKey clauSecreta = UtilitatsXifrar.keygenKeyGeneration(keySize);
+            else if(opcio == 4){
+                UtilitatsXifrar.desxifrarTextCodi112();
+            }
 
-        byte[] textXifrat = UtilitatsXifrar.encryptData(textClar,clauSecreta);
+            else if(opcio == 5){
+                UtilitatsXifrar.desxifrarTextAmagat();
+            }
 
-        byte[] textDesxifrat = UtilitatsXifrar.decryptData(textXifrat,clauSecreta);
+            else if(opcio == 0){
+                sortir = true;
+            }
+        }
 
-        System.out.println("Text xifrat: " + new String(textXifrat));
-        System.out.println("Text desxifrat: " + new String(textDesxifrat));
-    }
-
-    public static void xifrarDesxifrarCodi112() throws IOException {
     }
 }
--- src/UtilitatsXifrar.java
+++ src/UtilitatsXifrar.java
@@ -1,10 +1,16 @@
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -63,5 +69,108 @@ public class UtilitatsXifrar {
             System.err.println("Error desxifrant les dades: " + ex);
         }
         return decryptedData;
+    }
+
+    public static void xifrarDesxifrarCodi111() throws IOException {
+        String rutaArxiu = "src/clausA4.txt";
+
+        byte[] textClar = Files.readAllBytes(Paths.get(rutaArxiu));
+        int keySize = 128;
+
+
+        SecretKey clauSecreta = UtilitatsXifrar.keygenKeyGeneration(keySize);
+
+        byte[] textXifrat = UtilitatsXifrar.encryptData(textClar,clauSecreta);
+
+        byte[] textDesxifrat = UtilitatsXifrar.decryptData(textXifrat,clauSecreta);
+
+        System.out.println("Text xifrat: " + new String(textXifrat));
+        System.out.println("Text desxifrat: " + new String(textDesxifrat));
+    }
+
+    public static void xifrarDesxifrarCodi112() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String rutaArxiu = "src/clausA4.txt";
+        System.out.println("Posa la contrasenya que vulguis, es generara una clau a partir d'aquesta contrasenya");
+        String paraulaDePas = scanner.nextLine();
+
+        byte[] textClar = Files.readAllBytes(Paths.get(rutaArxiu));
+        int keySize = 128;
+
+        SecretKey clauSecreta = UtilitatsXifrar.passwordKeyGeneration(paraulaDePas, keySize);
+
+        byte[] textXifrat = UtilitatsXifrar.encryptData(textClar,clauSecreta);
+
+        byte[] textDesxifrat = UtilitatsXifrar.decryptData(textXifrat,clauSecreta);
+
+        System.out.println("Text xifrat: " + new String(textXifrat));
+        System.out.println("Text desxifrat: " + new String(textDesxifrat));
+    }
+
+
+    public static void methodsSecretKey(){
+        System.out.println("En aquest apartat es genera una clau secreta, i mostrarem el binari de la clau, el algoritme i el seu format");
+        int keySize = 128;
+        SecretKey clauSecreta = UtilitatsXifrar.keygenKeyGeneration(keySize);
+
+        byte[] clauCodejada = clauSecreta.getEncoded();
+        System.out.println("La clau secreta codejada en binari: " + Arrays.toString(clauCodejada));
+
+        String algoritme = clauSecreta.getAlgorithm();
+        System.out.println("L'algoritme utilitzat per a generar aquesta clau: " + algoritme);
+
+        String format = clauSecreta.getFormat();
+        System.out.println("El format de la clau generada es " + format);
+        System.out.println();
+    }
+
+    public static void desxifrarTextCodi112() throws IOException {
+        String rutaArxiu = "src/clausA4.txt";
+
+        byte[] textClar = Files.readAllBytes(Paths.get(rutaArxiu));
+        int keySize = 128;
+
+        SecretKey clauSecreta = UtilitatsXifrar.passwordKeyGeneration("paraulaCorrecta", keySize);
+
+        byte[] textXifrat = UtilitatsXifrar.encryptData(textClar,clauSecreta);
+
+        SecretKey clauSecretaIncorrecta = UtilitatsXifrar.passwordKeyGeneration("paraulaIncorrecta", keySize);
+        try {
+            byte[] textDesxifrat = UtilitatsXifrar.decryptData(textXifrat, clauSecretaIncorrecta);
+            System.out.println("Text desxifrat: " + new String(textDesxifrat));
+        } catch (Exception e) {
+            System.out.println("No li estas passant la clau correcta!");
+        }
+    }
+
+    public static void desxifrarTextAmagat() throws IOException {
+        String rutaFitxer = "src/textamagat.crypt";
+        byte[] textXifrat = null;
+        try {
+            textXifrat = Files.readAllBytes(Paths.get(rutaFitxer));
+        } catch (IOException e) {
+            System.err.println("No s'ha pogut llegir el fitxer de text xifrat: " + e);
+            return;
+        }
+
+        String rutaFitxerClaus = "src/clausA4.txt";
+        List<String> claus = null;
+        try {
+            claus = Files.readAllLines(Paths.get(rutaFitxerClaus));
+        } catch (IOException e) {
+            System.err.println("No s'ha pogut llegir el fitxer de claus: " + e);
+            return;
+        }
+
+        for (String contrasenya : claus) {
+            SecretKey clauSecreta = UtilitatsXifrar.passwordKeyGeneration(contrasenya, 128);
+            byte[] textDesxifrat = UtilitatsXifrar.decryptData(textXifrat, clauSecreta);
+            if (textDesxifrat != null) {
+                System.out.println("Contrasenya correcta: " + contrasenya);
+                System.out.println("Text desxifrat: " + new String(textDesxifrat));
+                return;
+            }
+        }
+        System.out.println("No s'ha trobat cap contrasenya correcta.");
     }
 }
